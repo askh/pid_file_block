a5d9ac4e2043a5051d98d6e6d244af7f5c3ef82f
Alexander Kharitonov <askh@askh.ru>
2019-08-05 00:02:27 +0300

    Draft

--- lib/pid_file_block/application.rb
+++ lib/pid_file_block/application.rb
@@ -9,30 +9,33 @@ class PidFileBlock
     def run_application
     end
     
-    def do_exit
-      @pid_file.release if @pid_file
-      exit @exit_code_normal
+    def do_exit(pid_file_block, exit_code)
+      pid_file_block.release if pid_file_block
+      exit exit_code
     end
 
-    def run(piddir:, pidfile:, exit_code_normal: 0, exit_code_process_exists_error: 1)
-      @exit_code_normal = exit_code_normal
-      @exit_code_process_exists_error = exit_code_process_exists_error
+    def run(piddir:,
+            pidfile:,
+            exit_code_normal: 0,
+            exit_code_process_exists_error: 1,
+            exit_code_interrupt: 2)
+      pid_file_block = nil
       old_term = Signal.trap('TERM') do
-        do_exit
+        do_exit(pid_file_block, exit_code_interrupt)
       end
       old_int = Signal.trap('INT') do
-        do_exit
+        do_exit(pid_file_block, exit_code_interrupt)
       end
-      @pid_file = PidFileBlock.new(piddir: piddir, pidfile: pidfile)
+      pid_file_block = PidFileBlock.new(piddir: piddir, pidfile: pidfile)
       begin
-        @pid_file.open do
+        pid_file_block.open do
           run_application
         end
-      rescue PidFileSimple::ProcessExistsError
-        STDERR.puts "Error: process exists (see pid in file #{pid_file.pid_file_full_name})."
-        exit @exit_code_process_exists_error
+      rescue PidFileBlock::ProcessExistsError
+        STDERR.puts "Error: process exists (see pid in file #{pid_file_block.pid_file_full_name})."
+        exit exit_code_process_exists_error
       end
-      exit @exit_code_normal
+      exit exit_code_normal
     end
     
   end
